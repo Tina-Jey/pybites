deb7eb1f58001feb62357209d82122c237de7b9b
Tina-Jey <jujurczak@gmail.com>
2024-05-27 19:45:55 +0100

    Commit 2

--- pybites_bite64/event.py
+++ pybites_bite64/event.py
@@ -1,12 +1,25 @@
-import itertools
+#import itertools
+
+#names = 'Tim Bob Julian Carmen Sofia Mike Kim Andre'.split()
+#locations = 'DE ES AUS NL BR US - - '.split()
+#confirmed = [False, True, True, False, True, '-', '-', '-']
+
+
+#def get_attendees():
+#    for participant in zip(names, locations, confirmed):
+#        print(participant)
+
+
+# better solution:
+from itertools import zip_longest
 
 names = 'Tim Bob Julian Carmen Sofia Mike Kim Andre'.split()
-locations = 'DE ES AUS NL BR US - - '.split()
-confirmed = [False, True, True, False, True, '-', '-', '-']
+locations = 'DE ES AUS NL BR US'.split()
+confirmed = [False, True, True, False, True]
 
 
 def get_attendees():
-    for participant in zip(names, locations, confirmed):
+    for participant in zip_longest(names, locations, confirmed, fillvalue='-'):
         print(participant)
 
 
